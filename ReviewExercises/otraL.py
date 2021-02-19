c9e0ed47647974ab6170d99c0bb8721415aca3f3
Asier <blazgar@gmail.com>
2021-02-19 09:08:11 +0100

    Update otraL.py

--- ReviewExercises/otraL.py
+++ ReviewExercises/otraL.py
@@ -1,4 +1,10 @@
 
+'''
+altura = int(input ('altura '))
+ancho = int(input ('ancho '))
+grosor=int(input ('grosor '))
+base = int(input ('base '))
+'''
 
 altura= 10
 ancho =5
@@ -6,17 +12,14 @@ grosor= 5
 base =10
 
 
-
+ancho1=ancho
 for i in range (altura):#filas
     for j in range (ancho):#columnas
-
-        if(i==altura-grosor-1):
-            ancho=base
-
-
-        if(i==0 or i==altura-1 or j==0 or j==ancho-1):
+        if(i==0 or i==altura-1 or j==0 or j==ancho-1 or ((i==altura-grosor)and j>=ancho1-1) ):
             print('* ', end='')
         else:
             print('  ', end='')
 
+    if(i==altura-grosor-1):
+            ancho=base
     print()
